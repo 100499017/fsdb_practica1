4b6c2d431f5ebef21c13386356402a42dcdc654a
AlbertoSocorro <alsocorrogallardo@gmail.com>
2025-02-28 18:16:49 +0100

    funcionan dos insert

--- NEWload.sql
+++ NEWload.sql
@@ -5,32 +5,55 @@
 --Cargamos los datos de libros. Usamos la funcion MIN() para que tome el primer valor no nulo que encuentre
 --en cada columna. De esta forma, si hay multiples filas con el mismo titulo y autor pero con valores diferentes en
 --otras columnas, se insertara una fila en la tabla libros con los valores de la primera fila que encuentre.
-/*
+
 INSERT INTO libros (titulo, autor_principal, pais_publicacion, lengua_original, fecha_publicacion,
     titulos_alternativos, tema, premios, otros_autores, mencion_autores, notas_contenido)
 SELECT
     p.title,
     p.main_author,
-    MIN(p.pub_country) AS pub_country,  -- Selecciona el primer valor (no NULL) de pub_country
-    MIN(p.main_language) AS main_language,  -- Selecciona el primer valor (no NULL) de main_language
-    TO_DATE(MIN(p.pub_date), 'YYYY') AS pub_date,  -- Elige la fecha más antigua
-    MIN(p.alt_title) AS alt_title,  -- Selecciona el primer valor de alt_title
-    MIN(p.topic) AS topic,  -- Selecciona el primer valor (no NULL) de topic
-    MIN(p.awards) AS awards,  -- Selecciona el primer valor (no NULL) de awards
-    MIN(p.other_authors) AS other_authors,  -- Selecciona el primer valor (no NULL) de other_authors
-    MIN(p.mention_authors) AS mention_authors,  -- Selecciona el primer valor (no NULL) de mention_authors
-    MIN(p.content_notes) AS content_notes  -- Selecciona el primer valor (no NULL) de content_notes
+    MIN(p.pub_country),
+    MIN(p.main_language),
+    MIN(TO_DATE(p.pub_date, 'YYYY')),
+    MIN(p.alt_title),
+    MIN(p.topic),
+    MIN(p.awards),
+    MIN(p.other_authors),
+    MIN(p.mention_authors),
+    MIN(p.content_notes)
 FROM fsdb.acervus p
-GROUP BY p.title, p.main_author
-HAVING NOT EXISTS (
-    SELECT 1
-    FROM libros l
-    WHERE l.titulo = p.title
-    AND l.autor_principal = p.main_author
-);
-*/
+WHERE NOT EXISTS (
+    SELECT 1 FROM libros l WHERE l.titulo = p.title AND l.autor_principal = p.main_author
+)
+GROUP BY p.title, p.main_author;
+
 --Cargamos los datos de ediciones, usando la misma logica que en el caso anterior con la funcion MIN()
 
+INSERT INTO ediciones (isbn, titulo, autor_principal, lengua_principal, otras_lenguas, edicion, editorial,
+    extension, serie, deposito_legal, lugar_publicacion, dimensiones, otras_caracteristicas, material_ajeno,
+    notas, id_biblioteca_nacional, url_edicion)
+SELECT
+    p.isbn,
+    MIN(p.title),
+    MIN(p.main_author),
+    MIN(p.main_language),
+    MIN(p.other_languages),
+    MIN(p.edition),
+    MIN(p.publisher),
+    MIN(p.extension),
+    MIN(p.series),
+    MIN(p.copyright),
+    MIN(p.pub_place),
+    MIN(p.dimensions),
+    MIN(p.physical_characteristics),
+    MIN(p.attached_materials),
+    MIN(p.notes),
+    MIN(p.national_lib_id),
+    MIN(p.url),
+FROM fsdb.acervus p
+WHERE NOT EXISTS (
+    SELECT 1 FROM ediciones e WHERE e.isbn = p.isbn
+)
+GROUP BY p.isbn;
 
 --Cargamos los datos de ejemplares, usando la misma logica que en el caso anterior con la funcion MIN()
 
